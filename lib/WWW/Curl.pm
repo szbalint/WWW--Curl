233e865b4137020b15d39e5f81659b6bb4e9525c
“Bálint Szilakszi” <szbalint@cpan.org>
2009-12-15 18:08:39 +0100

    Merging some documentation fixes from FreeBSD (thanks Peter)

--- lib/WWW/Curl.pm
+++ lib/WWW/Curl.pm
@@ -311,7 +311,7 @@ Version 3.02 adds some backwards compatibility for scripts still using
 
 Version 3.01 added some support for pre-multi versions of libcurl.
 
-Version 3.00 adds WWW::Curl::Multi interface, and a new module names
+Version 3.00 adds WWW::Curl::Multi interface, and new module names
 following perl conventions (WWW::Curl::Easy rather than WWW::Curl::easy),
 by Sebastian Riedel <sri at cpan.org>.
 
